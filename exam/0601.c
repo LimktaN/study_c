f486e1c596fa2defae0aadefb55d2d966d99e43b
LimktaN <qz2012@naver.com>
2022-12-25 11:53:21 +0900

    TBC 6-1 exam

--- exam/0601.c
+++ exam/0601.c
@@ -3,12 +3,12 @@
 int main()
 {
     int num = 0, sum = 0;
+    printf("수를 입력해주세요 : ");
 
-    while(sum < 10000)
+    while(scanf("%d", &num) == 1)
     {
-        printf("수를 입력해주세요 : ");
-        scanf("%d", &num);
         sum += num;
+        printf("수를 입력해주세요 : ");
     }
 
     printf("입력한 수들의 총합은 %d입니다.\n", sum);
