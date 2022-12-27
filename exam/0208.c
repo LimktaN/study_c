7879239e9d7b08ca59cf890fd44100c589c72c34
LimktaN <qz2012@naver.com>
2022-12-27 23:46:48 +0900

    TBC 2-8 exam

---
+++ exam/0208.c
@@ -0,0 +1,15 @@
+// https://en.cppreference.com/w/c/keyword
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    /*
+    함수명은 변수명으로 사용하지 않는 것을 권장
+    int printf = 1;
+    int bool = 2;
+    */
+
+    return 0;
+}
