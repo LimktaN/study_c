3440c44891d212594290a28ae051f284d4164ee2
LimktaN <qz2012@naver.com>
2022-12-27 23:41:05 +0900

    TBC 2-7 exam

---
+++ exam/0207.c
@@ -0,0 +1,25 @@
+/*
+ *  Written by LimktaN
+ */
+
+
+#include <stdio.h>
+
+int main()
+{
+    /*
+        여러줄 주석 처리
+        This is my code
+    */
+    // 한줄 주석 처리
+    int x, y, sum;
+
+    x = 1;
+    y = 4;
+
+    sum = x + y;
+
+    printf("%i + %i = %i\n", x, y, sum);
+
+    return 0;
+}
